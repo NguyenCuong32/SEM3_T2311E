c31a9606bf0af31dcdbc84bf45257ad6a3113bde
greenwormstudio <greenwormstudio@gmail.com>
2025-01-08 19:22:21 +0700

    Demo Mongo db

---
+++ LDM/mongodb_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging.Internal;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string connnectionString = "mongodb://root:123456@127.0.0.1:27017/";
+        var client = new MongoClient(connnectionString);
+        var colletions = client.GetDatabase("DemoDB").GetCollection<BsonDocument>("Users");
+        if (colletions!=null)
+        {
+            foreach (var item in colletions.Aggregate<BsonDocument>().ToList())
+            {
+                System.Console.WriteLine(item["username"]);
+                System.Console.WriteLine(item["fullname"]);
+            }
+        }
+    }
+}
